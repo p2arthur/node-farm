04c391953c2b5168396839937bb6326e36966f85
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-25 20:59:39 -0300

    Add new module for the replaceTemplate function

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 //Server
 
 //Top level code once the code is loaded
 
 //Creating a replace element function to replace the placeholders on the overview html with actual data
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
 
 //Reading the templates when the file is first loaded
 const tempOverview = fs.readFileSync(
